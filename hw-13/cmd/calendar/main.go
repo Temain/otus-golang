71f777b1a21f5c8a09ab81a9c226389df5ebdf43
artyom.aseev@proficlicks.ru <temain@mail.ru>
2020-02-09 17:15:57 +0300

    init

--- hw-13/cmd/calendar/main.go
+++ hw-13/cmd/calendar/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"time"
 
+	c "github.com/temain/otus-golang/hw-13/pkg/calendar"
+)
+
+func main() {
+	var calendar c.Calendar
+	calendar = c.NewCalendar()
+
+	// Добавление нового события.
+	event := &c.Event{
+		Id:          1,
+		Title:       "Morning coffee",
+		Description: "The most important event of the day",
+		Date:        time.Now(),
+	}
+	_ = calendar.Add(event)
 }
