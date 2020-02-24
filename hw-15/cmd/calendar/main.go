a59f92ae3f295d4d044835afa517d6b410cff39f
temain <asv.artm@gmail.com>
2020-02-25 00:15:09 +0300

    Config

--- hw-15/cmd/calendar/main.go
+++ hw-15/cmd/calendar/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"time"
 
+	"github.com/heetch/confita"
+	"github.com/heetch/confita/backend/file"
+	"github.com/heetch/confita/backend/flags"
+	"github.com/spf13/pflag"
+	"github.com/temain/otus-golang/hw-15/configs"
 	c "github.com/temain/otus-golang/hw-15/pkg/calendar"
 )
 
 func main() {
+	cfg := readConfig()
 	calendar := c.NewCalendar()
 
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
@@ -25,8 +32,29 @@ func main() {
 		_ = calendar.Add(event)
 		log.Println("added new event")
 	})
-	err := http.ListenAndServe(":80", nil)
+	err := http.ListenAndServe(cfg.HttpListen, nil)
 	if err != nil {
 		log.Fatalf("http server error: %v", err)
 	}
+}
+
+func readConfig() *configs.Config {
+	var configPath string
+	pflag.StringVarP(&configPath, "config", "c", "configs/config.json", "Config file path")
+
+	loader := confita.NewLoader(
+		file.NewBackend(configPath),
+		flags.NewBackend(),
+	)
+	cfg := configs.Config{}
+	err := loader.Load(context.Background(), &cfg)
+	if err != nil {
+		log.Fatalf("read config error: %v", err)
+	}
+
+	return &cfg
+}
+
+func configureLogging() {
+
 }
--- hw-15/go.mod
+++ hw-15/go.mod
@@ -1,3 +1,8 @@
 module github.com/temain/otus-golang/hw-15
 
 go 1.13
+
+require (
+	github.com/heetch/confita v0.9.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
