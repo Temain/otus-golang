6e4fbdbbcdb9a3b2b9d9867c6cbd8e5e1894c99e
Temain <asv.artm@gmail.com>
2020-05-04 18:47:48 +0300

    init, grpc, tests

--- project/tests/rotation_grpc_test.go
+++ project/tests/rotation_grpc_test.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/cucumber/godog"
+	"github.com/cucumber/messages-go/v10"
 	"google.golang.org/grpc"
 
 	"github.com/Temain/otus-golang/project/pkg/proto"
 )
+
+var grpcListen = os.Getenv("TESTS_GRPC_API")
+
+func init() {
+	if grpcListen == "" {
+		grpcListen = "grpc_api:50052"
+	}
+}
 
 type rotationGrpcTest struct {
 	ctx        context.Context
@@ -24,6 +37,23 @@ type rotationGrpcTest struct {
 	//deleteNotExists    bool
 }
 
+func (test *rotationGrpcTest) connect(*messages.Pickle) {
+	test.ctx, _ = context.WithTimeout(context.Background(), 5*time.Minute)
+	cc, err := grpc.Dial(grpcListen, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	test.client = proto.NewRotationServiceClient(cc)
+	test.clientConn = cc
+}
+
+func (test *rotationGrpcTest) close(*messages.Pickle, error) {
+	err := test.clientConn.Close()
+	if err != nil {
+		fmt.Errorf("error on close connection: %v", err)
+	}
+}
+
 func (test *rotationGrpcTest) iCallAddMethod() error {
 	return godog.ErrPending
 }
@@ -35,6 +65,10 @@ func (test *rotationGrpcTest) methodShouldReturnSuccessResult() error {
 func FeatureContext(s *godog.Suite) {
 	testGrpc := new(rotationGrpcTest)
 
+	s.BeforeScenario(testGrpc.connect)
+
 	s.Step(`^I call add method$`, testGrpc.iCallAddMethod)
 	s.Step(`^Method should return success result$`, testGrpc.methodShouldReturnSuccessResult)
+
+	s.AfterScenario(testGrpc.close)
 }
