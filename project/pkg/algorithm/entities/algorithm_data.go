e3d75467117a5fdd4a7e1a2e9450f28e575888d1
Temain <asv.artm@gmail.com>
2020-05-07 04:16:29 +0300

    algorithm, tests

--- project/pkg/algorithm/entities/algorithm_data.go
+++ project/pkg/algorithm/entities/algorithm_data.go
@@ -1,4 +1,7 @@
 package entities
 
 type AlgorithmData struct {
+	HandleId  int64 // идентификатор ручки
+	AvgIncome int64 // средний доход от ручки
+	Count     int64 // сколько раз мы дёрнули за ручку
 }
--- project/pkg/algorithm/multiarmed_bandit.go
+++ project/pkg/algorithm/multiarmed_bandit.go
@@ -1,6 +1,9 @@
 package algorithm
 
 import (
+	"fmt"
+	"math"
+
 	"github.com/Temain/otus-golang/project/pkg/algorithm/entities"
 	"github.com/Temain/otus-golang/project/pkg/algorithm/interfaces"
 )
@@ -12,6 +15,29 @@ func NewMultiarmedBandit() (interfaces.RotationAlgorithm, error) {
 	return &MultiarmedBandit{}, nil
 }
 
-func (mb *MultiarmedBandit) GetHandle([]entities.AlgorithmData) (int64, error) {
-	return 0, nil
+func (mb *MultiarmedBandit) GetHandle(items []entities.AlgorithmData) (int64, error) {
+	var countTotal int64
+	for _, item := range items {
+		countTotal += item.Count
+	}
+
+	var handleId int64
+	var maxResult float64
+	for _, item := range items {
+		result := float64(item.AvgIncome) + math.Sqrt(2*math.Log(2)/float64(countTotal))
+		if result > maxResult {
+			maxResult = result
+			handleId = item.HandleId
+		}
+
+		fmt.Printf("handleId: %v, priority: %v\n", item.HandleId, result)
+	}
+
+	if math.IsInf(maxResult, 0) {
+		return 0, fmt.Errorf("wrong result of algorthm, priority is infinity")
+	}
+
+	fmt.Printf("Handle with max priority: %v, priority: %v\n", handleId, maxResult)
+
+	return handleId, nil
 }
